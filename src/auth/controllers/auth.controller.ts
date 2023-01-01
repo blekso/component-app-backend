1eed6227ddc466f820e481e7740201f32fa94c22
blekso <blekepix@outlook.com>
2023-01-01 23:24:34 +0100

    fixed auth docs

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AuthService } from '../auth.service';
 import { SignInDto, SignUpDto } from '../dto';
 
@@ -7,12 +8,20 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({
+    tags: [`Auth`],
+    summary: 'Regsiter',
+  })
   signup(@Body() dto: SignUpDto) {
     return this.authService.signup(dto);
   }
 
-  @HttpCode(HttpStatus.OK)
   @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    tags: [`Auth`],
+    summary: 'Login',
+  })
   signin(@Body() dto: SignInDto) {
     return this.authService.signin(dto);
   }
